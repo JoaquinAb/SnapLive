0bc7854db7b142b8f9c18f4980d350efdc1606f5
JoaquinAb <joaquinabreu14@hotmail.com>
2026-02-09 18:17:14 -0300

    fix(db): add ssl config for production

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,9 +1,17 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: process.env.NODE_ENV === 'development' ? console.log : false,
+  dialectOptions: isProduction ? {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  } : {},
   pool: {
     max: 5,
     min: 0,
